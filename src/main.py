954fa6ee16c9cf298f8a3bb07f2435c0b3110c28
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-25 19:56:12 +0100

    event motions

--- src/main.py
+++ src/main.py
@@ -13,14 +13,38 @@ class Main:
 
   def mainloop(self):
     game = self.game
+    board = self.game.board
     screen = self.screen
+    dragger = self.game.dragger
 
     while True:
       game.show_bg(screen)
       game.show_pieces(screen)
 
+      if dragger.dragging:
+        dragger.update_blit(screen)
+
       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          dragger.update_mouse(event.pos)
+
+          clicked_row = dragger.mouseY // SQSIZE
+          clicked_col = dragger.mouseX // SQSIZE
+
+          if board.squares[clicked_row][clicked_col].has_piece():
+            piece = board.squares[clicked_row][clicked_col].piece
+            dragger.save_initial(event.pos)
+            dragger.drag_piece(piece)
+            
+        elif event.type == pygame.MOUSEMOTION:
+          if dragger.dragging:
+            dragger.update_mouse(event.pos)
+            dragger.update_blit(screen)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+          dragger.undrag_piece()
+        
+        elif event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
       
